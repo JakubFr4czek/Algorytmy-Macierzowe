b2b458c26bcbfaef2f861c36cd601f4101c14868
JakubFr4czek <jakubfraczek.m8@gmail.com>
2024-10-20 15:18:02 +0200

    Working on lab1 fix

--- Lab1/utility.py
+++ Lab1/utility.py
@@ -5,10 +5,9 @@ lower_bound = 0.00000001
 upper_bound = 1.0
 epsilon = 1e-10 
 
-def get_random_matrices(n = 1):
+def get_random_matrices_power_of_2(n = 1):
 
     #creating different size matrices, two of each with values in the open range (0.00000001, 1.0)
-    
     matrices=[]
     for i in range (n):
         matrices.append([np.random.uniform(lower_bound + epsilon, upper_bound,(2**(i+1),2**(i+1))),np.random.uniform(lower_bound + epsilon, upper_bound,(2**(i+1),2**(i+1)))])
@@ -20,20 +19,23 @@ def get_random_matrices_any_size(n = 1):
     #creating different size matrices, two of each with values in the open range (0.00000001, 1.0)
     
     matrices=[]
-    for i in range (1, n):
-        matrices.append([np.random.uniform(lower_bound + epsilon, upper_bound,(i, i)),np.random.uniform(lower_bound + epsilon, upper_bound,(i, i))])
+    for i in range (n):
+        matrices.append([np.random.uniform(lower_bound + epsilon, upper_bound,(i + 1, i + 1)),np.random.uniform(lower_bound + epsilon, upper_bound,(i + 1, i + 1))])
 
     return matrices
 
 def get_random_matrix_pair_any_size(n = 1):
     return np.random.uniform(lower_bound + epsilon, upper_bound,(n, n)),np.random.uniform(lower_bound + epsilon, upper_bound,(n, n))
 
-def pad_matrix(m):
-    # Calculating closes higher power of 2
-    power_of_2 = 2**math.ceil(math.log2(m.shape[0]))
-    padded_m = np.zeros((power_of_2, power_of_2))
-    padded_m[:m.shape[0], :m.shape[0]] = m
-    return padded_m
+def pad_matrix_even(m):
+    if m.shape[0] % 2 != 0:
+        m = np.vstack((m, np.zeros(shape=(1, m.shape[1]))))
+        
+    if m.shape[1] % 2 != 0:
+        m = np.hstack((m, np.zeros(shape=(m.shape[0], 1))))
+        
+    return m
+    
 
 def unpad_matrix(m, prev_shape):
     m = m[:prev_shape[0], :prev_shape[1]]
