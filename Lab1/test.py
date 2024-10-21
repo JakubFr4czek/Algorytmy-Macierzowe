296f1e8ac53f1fcf79dcf497e036e4618b6d1bf9
JakubFr4czek <jakubfraczek.m8@gmail.com>
2024-10-21 12:17:10 +0200

    Finished lab1 fix

--- Lab1/test.py
+++ Lab1/test.py
@@ -2,6 +2,15 @@ from utility import pad_matrix_even, get_random_matrix_pair_any_size, unpad_matr
 from matrix_mult import binet
 import numpy as np
 
-A, B = get_random_matrix_pair_any_size(255)
+
+A, B = get_random_matrix_pair_any_size(5)
+
+
 
 C1 = binet(A, B)
+
+print(C1)
+
+
+C2 = A@B
+print(C2)
