a6dde398be0f431bd400a5075eee0dcf0f0d91a3
JakubFr4czek <jakubfraczek@student.agh.edu.pl>
2024-10-21 10:06:12 +0200

    Further work on lab1 fix

--- Lab1/test.py
+++ Lab1/test.py
@@ -1,18 +1,7 @@
-from utility import pad_matrix_even, pad_matrix_to_2nd_power, get_random_matrix_pair_any_size, unpad_matrix
-from matrix_mult import strassen2, strassen
+from utility import pad_matrix_even, get_random_matrix_pair_any_size, unpad_matrix
+from matrix_mult import binet
 import numpy as np
 
-A, B = get_random_matrix_pair_any_size(46)
+A, B = get_random_matrix_pair_any_size(255)
 
-C1 = strassen2(A, B)
-
-original_shape = A.shape
-
-A = pad_matrix_to_2nd_power(A)
-B = pad_matrix_to_2nd_power(B)
-
-C2 = strassen(A, B)
-
-C2 = unpad_matrix(C2, original_shape)
-
-print(np.allclose(C1, C2, atol=1e-5))
+C1 = binet(A, B)
